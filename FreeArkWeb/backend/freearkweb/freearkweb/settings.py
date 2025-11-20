d319005c003cb0eae4048ee41fa74fdcd768906c
Yang Yang <55086934@qq.com>
2025-11-21 01:18:39 +0800

    fix(settings): 修改CORS配置为允许所有来源
    将CORS_ALLOWED_ORIGINS替换为CORS_ALLOW_ALL_ORIGINS以简化跨域请求配置

--- FreeArkWeb/backend/freearkweb/freearkweb/settings.py
+++ FreeArkWeb/backend/freearkweb/freearkweb/settings.py
@@ -180,8 +180,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS配置
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:8080,http://127.0.0.1:8080,http://192.168.31.52:8080，http://et116374mm892.vicp.fun').split(',')
+# CORS配置 - 允许所有来源
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE',
